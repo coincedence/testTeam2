33dbe46481d8ec65dbee85056b1484beb885d0b6
jbiz <pozolotusiko@mail.ru>
2022-10-25 21:05:03 +0300

    minor changes

--- src/test/java/com/mycompany/app/UserPage.java
+++ src/test/java/com/mycompany/app/UserPage.java
@@ -7,7 +7,8 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class UserPage {
     private final SelenideElement MusicButton = $(By.xpath("//*[@id=\"music_toolbar_button\"]/div[2])"));
-    private final SelenideElement SongName = $(By.xpath("//*[@id=\"music_toolbar_button\"]/div[2])"));
+    private final SelenideElement SongName = $(By.xpath("//*[@class=\"feed-w\"]" +
+            "//*[@class=\"track-with-cover_line\"]//*[@itemprop=\"name\"]"));
     /**
      * Clicking on music button
      */
